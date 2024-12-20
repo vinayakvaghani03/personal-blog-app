bf0d683fe119cdbc801c5ffb545c17d33145740d
vinayakvaghani03 <vinayakwork8@gmail.com>
2024-12-20 19:47:39 +0530

    connect-DB

--- back-end/app.js
+++ back-end/app.js
@@ -1,4 +1,9 @@
 const express = require("express")
 const app=express();
+const cors = require("cors")
+require("dotenv").config();
+require("./conn/conection")
+app.use(cors());
+app.use(express.json());
 
-app.listen(1000,()=>{console.log("Server is running")})
+app.listen(process.env.PORT,()=>{console.log("Server is running")});
---
+++ back-end/conn/conection.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+
+const conn = async () =>{
+    try {
+        await mongoose.connect(`${process.env.URL}`).then(()=> console.log("Connected to DB"));
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+conn();
